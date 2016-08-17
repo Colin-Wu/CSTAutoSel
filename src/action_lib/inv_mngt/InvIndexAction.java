de0dc642251ec01c961d4f9362e393e161ad9cfd
SunilPrabhu <sunil.prabhu@hpe.com>
2016-08-17 10:23:57 +0530

    Selenium_TestCases
    Selenium_TestCases  for InventoryLookup,Receiving Putaway, POMngtAction

--- src/action_lib/inv_mngt/InvIndexAction.java
+++ src/action_lib/inv_mngt/InvIndexAction.java
@@ -24,4 +24,74 @@ public class InvIndexAction {
 		CmdDocReceiving.click();
 		
 	}
+	
+    public void GotoReceivingPutaway () throws NoSuchElementException {
+		
+    	InvIndexObj invIdxObj = new InvIndexObj(webdriver);
+    	
+		WebElement CmdReceivingPutaway = invIdxObj.getCmdReceivingPutawayLocator();
+
+		CmdReceivingPutaway.click();
+		
+	}
+
+    public void GotoInventoryLookup () throws NoSuchElementException {
+	
+    	InvIndexObj invIdxObj = new InvIndexObj(webdriver);
+    	
+		WebElement CmdInventoryLookup = invIdxObj.getCmdInventoryLookupLocator();
+
+		CmdInventoryLookup.click();
+	
+     }
+	 
+    public void GotoPickAdmin () throws NoSuchElementException {
+    	
+    	InvIndexObj invIdxObj = new InvIndexObj(webdriver);
+    	
+		WebElement CmdPickAdmin = invIdxObj.getCmdPickAdminLocator();
+
+		CmdPickAdmin.click();
+    	
+    }
+    
+    public void GotoPickQueue () throws NoSuchElementException {
+    	
+    	InvIndexObj invIdxObj = new InvIndexObj(webdriver);
+    	
+		WebElement CmdPickQueue = invIdxObj.getCmdPickQueueLocator();
+
+		CmdPickQueue.click();
+    	
+    }
+    
+    public void GotoTransferRequest () throws NoSuchElementException {
+    	
+    	InvIndexObj invIdxObj = new InvIndexObj(webdriver);
+    	
+		WebElement CmdTransferRequest = invIdxObj.getCmdTransferRequestLocator();
+
+		CmdTransferRequest.click();
+    	
+    }
+
+    public void GotoTransferAdmin () throws NoSuchElementException {
+    	
+    	InvIndexObj invIdxObj = new InvIndexObj(webdriver);
+    	
+		WebElement CmdTransferAdmin = invIdxObj.getCmdTransferAdminLocator();
+
+		CmdTransferAdmin.click();
+    	
+    }
+    
+    public void GotoTransferAction () throws NoSuchElementException {
+    	
+    	InvIndexObj invIdxObj = new InvIndexObj(webdriver);
+    	
+		WebElement CmdTransferAction = invIdxObj.getCmdDocReceivingLocator();
+
+		CmdTransferAction.click();
+    }
+      
 }
